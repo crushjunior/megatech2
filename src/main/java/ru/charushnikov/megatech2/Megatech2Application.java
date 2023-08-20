9390002dec374d253565721673d850320cc5c008
crushjunior <charushnikovvadim@gmail.con>
2023-08-20 18:44:34 +0300

    Initial commit

---
+++ src/main/java/ru/charushnikov/megatech2/Megatech2Application.java
@@ -0,0 +1,13 @@
+package ru.charushnikov.megatech2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Megatech2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Megatech2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/charushnikov/megatech2/Megatech2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.charushnikov.megatech2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Megatech2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
