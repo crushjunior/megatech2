f69b68edc1b0d997717ac2f34fdbc30bad99797b
crushjunior <charushnikovvadim@gmail.con>
2023-08-20 19:30:03 +0300

    абстракции entity, service, repository

---
+++ src/main/java/ru/charushnikov/megatech2/entity/AbstractEntity.java
@@ -0,0 +1,22 @@
+package ru.charushnikov.megatech2.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
